c432bcc515f8444fb0da2d9a4cd142a9eb9a1f8b
BikyC <61280553+bbreunig@users.noreply.github.com>
2023-09-15 14:56:34 +0200

    Delete start.bat

--- start.bat
+++
@@ -1,5 +0,0 @@
-cd /d "C:\Users\rawex\Documents\LabRecorder\"
-start "" "C:\Users\rawex\Documents\LabRecorder\LabRecorder.exe"
-
-cd /d "C:\Users\rawex\Documents\PolarBand2lsl-main\"
-"C:\Users\rawex\anaconda3\python.exe" "C:\Users\rawex\Documents\PolarBand2lsl-main\Polar2LSL.py"
